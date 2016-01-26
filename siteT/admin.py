f4dc18afc6b7d7eb5aea7de20a3c225debad8a78
LavoisierM <lavoisier.f.m@gmail.com>
2016-01-26 02:11:09 -0300

    Criacao basica das Classes Cliente, evento e usuario no admin

--- siteT/admin.py
+++ siteT/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post
+from .models import Evento
+from .models import Cliente
+from .models import Usuario
 
 
 admin.site.register(Post)
+admin.site.register(Evento)
+admin.site.register(Cliente)
+admin.site.register(Usuario)
+
--- siteT/models.py
+++ siteT/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django import forms
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -16,3 +17,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Evento(models.Model):
+    cod = models.CharField(max_length=2)
+    titulo = models.CharField(max_length=10)
+    data = models.DateTimeField(blank=True, null=True)
+    descricao = models.TextField(max_length=200)
+    responsavel = models.ForeignKey('auth.User')
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=20)
+    dataNascimento = models.DateField(blank=True, null=True)
+    CPF = models.CharField(max_length=8)
+    endereco = models.CharField
+
+class Usuario(models.Model):
+    name_user = models.CharField(max_length=20)
+    email_user = models.EmailField()
+    senha = models.CharField(max_length=5)
